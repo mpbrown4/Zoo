fa5a5395eb80b3ac12dcd2904c7ae2ac553439c4
kjkleindorfer <kjkleindorfer@dmacc.edu>
2018-03-14 14:01:28 -0500

    Initial project setup

---
+++ src/AnimalNoise.java
@@ -0,0 +1,13 @@
+import model.Cat;
+
+public class AnimalNoise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat fluffy = new Cat();
+		System.out.println(fluffy.speak());
+		
+		
+	}
+
+}
