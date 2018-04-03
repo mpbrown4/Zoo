63ff9809a49ce3ff17d51c3c1832cb24164c80f1
mpbrown4 <mpbrown4@users.noreply.github.com>
2018-04-03 17:32:45 -0500

    Added dog.

---
+++ src/model/Dog.java
@@ -0,0 +1,42 @@
+// Madison's new animal
+package model;
+
+public class Dog {
+	private String name;
+	private int distance;
+	private int age;
+	
+	public Dog() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Dog(String name, int age, int distance) {
+		super();
+		this.name = name;
+		this.distance = distance;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String speak() {
+		return "Bork! Boorrkk!! Booorrrkkkk!!!";
+	}
+
+}
